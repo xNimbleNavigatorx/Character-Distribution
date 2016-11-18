3fc0a64444d605a2cfcdad7a49ffcf54dc2cd729
xNimbleNavigatorx <alexandrehankel@hanovernorwichschools.org>
2016-11-17 23:51:13 -0500

    Updated from Brython Server: 11/17/2016 11:51:10 PM

--- distribution.py
+++ distribution.py
@@ -1,6 +1,6 @@
 """
 distribution.py
-Author: <your name here>
+Author: xNimbleNavigatorx
 Credit: <list sources used, if any>
 
 Assignment:
@@ -36,3 +36,7 @@ Notice about this example:
   in the text and they are listed in the output in alphabetical order.
 * Letters that do not occur in the text are not listed in the output at all.
 """
+
+s = input("Please enter a string of text (the bigger the better): ")
+print('The distribution of characters in "' + s + '" is:')
+
